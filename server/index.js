921e68318453b219c0e7bd669cdc07f1642302c0
krokedilmartin <84283336+krokedilmartin@users.noreply.github.com>
2023-05-04 19:31:43 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const upload = multer({ dest: 'uploads/' }); // specify the upload directory
-const cors = require('cors');
 app.use(cors()); // enable CORS for all routes
-
 
 app.post('/upload', upload.single('image'), (req, res) => {
   const file = req.file;
